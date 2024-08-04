8b9784c57e45c7c58fe3b48e67657e52bd66497e
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-04 21:31:27 +0100

    Save Bet Count to Database

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,37 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Card.css'
 import more from './more.png'
 import { Link } from 'react-router-dom'
+import { supabase } from '../client'
 
 
 const Card = (props) =>  {
 
   const [count, setCount] = useState(0)
-  const updateCount = () => {
+
+  useEffect(() => {
+    const fetchCount = async () => {
+        const { data } = await supabase
+            .from('Posts')
+            .select('betCount')
+            .eq('id', props.id)
+            .single();
+        
+        setCount(data ? data.betCount : 0);
+    };
+
+    fetchCount();
+  }, [props.id]);
+
+  const updateCount = async (event) => {
+    event.preventDefault();
+  
+    await supabase
+      .from('Posts')
+      .update({ betCount: count + 1})
+      .eq('id', props.id)
+  
     setCount((count) => count + 1);
   }
 
